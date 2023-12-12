4313f0bf32092235e6f71ed3ab05f1659bcf87e9
matteomedori <matteo.medori.97@gmail.com>
2023-12-12 15:33:44 +0100

    feat: struttura cards component

---
+++ src/components/AppCard.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: "AppCard",
+};
+</script>
+
+<template>
+  <div class="card">
+    <div>img</div>
+    <div>title</div>
+    <div>type</div>
+  </div>
+</template>
+
+<style lang="scss">
+.card {
+  height: 100px;
+  background-color: red;
+  width: calc((100% - 100px) / 5);
+}
+</style>
