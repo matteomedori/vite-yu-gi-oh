0c8f6625089c50885557e7bb36b04e34bd26c77b
matteomedori <matteo.medori.97@gmail.com>
2023-12-12 15:49:20 +0100

    feat: file store.js creato

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,12 +1,24 @@
 <script>
 import AppSelect from "./AppSelect.vue";
 import AppCards from "./AppCards.vue";
+import axios from "axios";
+import { store } from "../store";
 
 export default {
   name: "AppMain",
   components: {
     AppSelect,
     AppCards,
+  },
+  data() {
+    return {
+      apiURL: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0",
+    };
+  },
+  created() {
+    axios.get(this.apiURL).then((response) => {
+      store.cards = response.data.data;
+    });
   },
 };
 </script>
